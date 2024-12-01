06a94cd732fb0695eae6735a49bbe6d4db9dace0
Martin Nicholson <recyclebing+github@gmail.com>
2024-12-01 14:15:51 +0100

    Day 01 Elixir

---
+++ README.md
@@ -0,0 +1,62 @@
+# Advent of Code 2024
+
+![](https://img.shields.io/badge/days%20completed-01-red) ![](https://img.shields.io/badge/stars%20⭐-02-yellow)
+
+https://adventofcode.com/2024
+
+| Puzzle | Difficulty | Fun | Completion | Python | Ruby | Elixir | PHP | JS | other |
+|--------|------------|-----|------------|:------:|:----:|:------:|:---:|:--:|:-----:|
+| [Day 1: Historian Hysteria](https://adventofcode.com/2024/day/1) | 1 | 2 | :star: :star: | - | - | :heart_decoration: **[01.exs](elixir/01.exs)** | - | - | - |
+
+_**Bold** = primary language for that day (others are ports)_
+
+## Elixir
+
+> Elixir Version: 1.14
+
+Run a solution
+
+```sh
+cd elixir
+elixir -r 01.exs -e Day01.part1
+```
+
+## Python
+
+> Python Version: 3.9
+
+Run a solution
+
+```sh
+cd python3
+python3 12.py
+```
+
+## PHP
+
+> PHP Version: 7.3
+
+Run a solution
+
+```sh
+cd php
+php 04.php
+```
+
+## NodeJS
+
+> Node Version: 18.12
+
+Run a solution
+
+```sh
+cd js
+node 07.js
+```
+
+## Ruby
+
+## TODO
+
+- document more comprehensively
+- visual output
---
+++ elixir/01.exs
@@ -0,0 +1,47 @@
+defmodule Day01 do
+  # Read input as lines
+  defp read_input do
+    File.read!(Path.expand("../inputs/input01.txt"))
+    |> String.split("\n")
+  end
+
+  # Convert lines to integer lists
+  defp to_integer_lists_by_column(input) do
+    lines = input
+    |> Enum.map(fn line ->
+        Regex.run(~r/^(\d+)\D+(\d+)$/, String.trim(line))
+        |> Enum.drop(1)
+        |> Enum.map(&String.to_integer/1)
+    end)
+
+    col1 = Enum.map(lines, fn [a, _] -> a end)
+    col2 = Enum.map(lines, fn [_, b] -> b end)
+    [col1, col2]
+  end
+
+  @doc """
+  Sort 2 lists ascending, find diff between each number pir, sum them.
+  """
+  def part1 do
+    [col1, col2] = to_integer_lists_by_column(read_input())
+    Enum.zip(Enum.sort(col1), Enum.sort(col2))
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum
+    |> IO.inspect(label: "P1")
+  end
+
+  @doc """
+  Find similarity: sum of count of col2 appearances of each col1 value
+  """
+  def part2 do
+    [col1, col2] = to_integer_lists_by_column(read_input())
+    col1
+    |> Enum.map(fn a -> a * Enum.count(col2, fn b -> b == a end) end)
+    |> IO.inspect()
+    |> Enum.sum
+    |> IO.inspect(label: "P2")
+  end
+end
+
+# P1: 2769675
+# P2: 24643097
